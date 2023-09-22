5fae3d8c6c49a159225a0136665af33078399297
davidpiar <thepineda@gmail.com>
2023-09-22 23:20:08 +0200

    Fixed sequential execution

--- src/extension.ts
+++ src/extension.ts
@@ -9,9 +9,10 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(vscode.commands.registerCommand('extension.runSequential', async () => {
 		const tasks = await getTasks();
-		for (const task of tasks) {
-			await vscode.tasks.executeTask(task);
-		}
+		// Assuming you provide a way to select tasks, for example "selectedTasks"
+		runTasksSequentially(tasks).catch(error => {
+			vscode.window.showErrorMessage(`Error running tasks: ${error.message}`);
+		});
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('extension.runParallel', async () => {
@@ -30,6 +31,30 @@ async function getTasks(): Promise<vscode.Task[]> {
 
 	// 3. Return the tasks that the user selected
 	return pickedTasks?.map(item => item.task) || [];
+}
+
+function runTasksSequentially(tasks: vscode.Task[]): Promise<void> {
+	return tasks.reduce((promiseChain, currentTask) => {
+		return promiseChain.then(() => runTask(currentTask));
+	}, Promise.resolve());
+}
+
+function runTask(task: vscode.Task): Promise<void> {
+	return new Promise((resolve, reject) => {
+		let disposable = vscode.tasks.onDidEndTask((event) => {
+			if (event.execution.task === task) {
+				disposable.dispose();
+				resolve();
+			}
+		});
+
+		vscode.tasks.executeTask(task).then((execution) => {
+			if (!execution) {
+				disposable.dispose();
+				reject(new Error("Failed to start the task"));
+			}
+		});
+	});
 }
 
 // This method is called when your extension is deactivated
